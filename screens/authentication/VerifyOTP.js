d8a8298fb7fb407e4a8ae2145385b5f25d2beeca
Imtiaz Chowdhury <imtiazschowdhury@gmail.com>
2021-01-27 10:55:04 +0600

    added fonts

---
+++ screens/authentication/VerifyOTP.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View } from "react-native";
+
+export const VerifyOTP = (props)=>{
+    return (
+        <View>
+            Vefiry
+        </View>
+    )
+}
+
+export default VerifyOTP
